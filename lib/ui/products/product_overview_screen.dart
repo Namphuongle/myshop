acefe74accbba325a49caf129805dbbc6bcf639f
duc <tanghuynh09@gmail.com>
2022-09-17 16:27:50 +0700

    xay dung trang tong quan cac san pham

---
+++ lib/ui/products/product_overview_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:myshop/ui/products/products_grid.dart';
+
+//import 'product_grid_tile.dart';
+
+enum FilterOptions { favorite, all }
+
+class ProductsOverviewScreen extends StatefulWidget {
+  const ProductsOverviewScreen({super.key});
+
+  @override
+  State<ProductsOverviewScreen> createState() => _ProductsOverViewScreenState();
+}
+
+class _ProductsOverViewScreenState extends State<ProductsOverviewScreen> {
+  var _showOnlyFavorites = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MyShop'),
+        actions: <Widget>[
+          buildProductFilterMenu(),
+          buildShoppingCarIcon(),
+        ],
+      ),
+      body: ProductsGrid(_showOnlyFavorites),
+    );
+  }
+
+  Widget buildShoppingCarIcon() {
+    return IconButton(
+      icon: const Icon(
+        Icons.shopping_cart,
+      ),
+      onPressed: () {
+        print('Go to cart screen');
+      },
+    );
+  }
+
+  Widget buildProductFilterMenu() {
+    return PopupMenuButton(
+      onSelected: (FilterOptions selectedValue) {
+        setState(() {
+          if (selectedValue == FilterOptions.favorite) {
+            _showOnlyFavorites = true;
+          } else {
+            _showOnlyFavorites = false;
+          }
+        });
+      },
+      icon: const Icon(
+        Icons.more_vert,
+      ),
+      itemBuilder: (ctx) => [
+        const PopupMenuItem(
+          value: FilterOptions.favorite,
+          child: Text('Only favorites'),
+        ),
+        const PopupMenuItem(
+          value: FilterOptions.all,
+          child: Text('show All'),
+        ),
+      ],
+    );
+  }
+}
