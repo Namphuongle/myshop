570e14a6ad2d0149cc79fb7b9c8f57923089bcd9
phuongNam <lephuongnam11062001@gmail.com>
2022-10-23 14:15:04 +0700

    Quan ly trang thai voi provider - ProductsManager

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,8 @@ import 'ui/orders/orders_screen.dart';
 
 import 'ui/screens.dart';
 
+import 'package:provider/provider.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -22,39 +24,46 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My Shop',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: 'Lato',
-        colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: Colors.purple,
-        ).copyWith(
-          secondary: Colors.deepOrange,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => ProductsManager(),
+        )
+      ],
+      child: MaterialApp(
+        title: 'My Shop',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          fontFamily: 'Lato',
+          colorScheme: ColorScheme.fromSwatch(
+            primarySwatch: Colors.purple,
+          ).copyWith(
+            secondary: Colors.deepOrange,
+          ),
         ),
+        home: const ProductsOverviewScreen(),
+        routes: {
+          CartScreen.routeName: (ctx) => const CartScreen(),
+          OrderScreen.routeName: (ctx) => const OrderScreen(),
+          UserProductsScreen.routeName: (ctx) => const UserProductsScreen(),
+        },
+        onGenerateRoute: (settings) {
+          if (settings.name == ProductDetailScreen.routeName) {
+            final productId = settings.arguments as String;
+            return MaterialPageRoute(
+              builder: (ctx) {
+                return ProductDetailScreen(
+                  ctx.read<ProductsManager>().findById(productId),
+                );
+              },
+            );
+          }
+
+          return null;
+        },
+
+        // child: ProductsOverviewScreen(),
       ),
-      home: const ProductsOverviewScreen(),
-      routes: {
-        CartScreen.routeName: (ctx) => const CartScreen(),
-        OrderScreen.routeName: (ctx) => const OrderScreen(),
-        UserProductsScreen.routeName: (ctx) => const UserProductsScreen(),
-      },
-      onGenerateRoute: (settings) {
-        if (settings.name == ProductDetailScreen.routeName) {
-          final productId = settings.arguments as String;
-          return MaterialPageRoute(
-            builder: (ctx) {
-              return ProductDetailScreen(
-                ProductsManager().findById(productId),
-              );
-            },
-          );
-        }
-
-        return null;
-      },
-
-      // child: ProductsOverviewScreen(),
     );
   }
 }
--- lib/ui/products/products_grid.dart
+++ lib/ui/products/products_grid.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 import 'product_grid_tile.dart';
 import 'products_manager.dart';
 
+import 'package:provider/provider.dart';
+
+import '../../models/product.dart';
+
 class ProductsGrid extends StatelessWidget {
   final bool showFavorites;
 
@@ -10,9 +14,13 @@ class ProductsGrid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final productsManager = ProductsManager();
-    final products =
-        showFavorites ? productsManager.favoriteItems : productsManager.items;
+    //Đọc ra danh sách các product sẽ được hiển thị từ ProductsManager
+
+    final products = context.select<ProductsManager, List<Product>>(
+        (productsManager) => showFavorites
+            ? productsManager.favoriteItems
+            : productsManager.items);
+
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
       itemCount: products.length,
